7c29e2874c290169918967c3ed875910797ea987
Raselj71 <raselj44@gmail.com>
2024-08-30 16:39:52 +0600

    fixing error

--- components/Megamenu.tsx
+++ components/Megamenu.tsx
@@ -7,7 +7,7 @@ interface propstype {
 }
 function Megamenu({ image, heading, des }: propstype) {
   return (
-    <div className="w-[170px] h-[170px] lg:w-[277px] lg:h-[300px]  md:w-[210px] md:h-[210px] px-4 py-2   lg:p-4 lg:py-2 md:p-8 drop-shadow-2xl rounded-[30px] bg-white flex flex-col justify-center items-center">
+    <div className="w-[150px] h-[150px] lg:w-[277px] lg:h-[300px]  md:w-[210px] md:h-[210px] px-2 py-2   lg:p-4 lg:py-2 md:p-8 drop-shadow-2xl rounded-[30px] bg-white flex flex-col justify-center items-center">
       <img src={image} alt="alt" className="size-7 md:size-auto" />
       <h2 className="text-[#5FE26C] text-[12px] uppercase font-bold lg:text-[24px] mt-2 md:mt-5 text-center">
         {heading}
--- pages/ServiceLayout.tsx
+++ pages/ServiceLayout.tsx
@@ -29,7 +29,7 @@ function ServiceLayout() {
           </div>
         </div>
 
-        <div className="grid grid-cols-2 gap-5 md:gap-5 lg:gap-7 mt-8 ">
+        <div className="w-full grid grid-cols-2 gap-2 md:gap-5 lg:gap-7 mt-8 ">
           <Megamenu
             image={"/salad.png"}
             heading={"catering"}
